661306b0081abc426fcdd6ec2c3bba935f2c0299
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-09-04 09:24:17 +0300

    AddressInfo class generated with xsd exe

--- src/DataTypeConvertorAPI.Test/DataTypeConvertorServiceTests.cs
+++ src/DataTypeConvertorAPI.Test/DataTypeConvertorServiceTests.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -22,24 +23,43 @@ namespace DataTypeConvertorAPI.Test
     [TestFixture]
     public class DataTypeConvertorHelperTests
     {
+        private string TestFilesPath;
+
+        [SetUp]
+        public void Setup()
+        {
+            TestFilesPath = ConfigurationManager.AppSettings["TestFiles"];
+        }
+
         [Test]
         public void should_generate_xml_from_csv()
         {
             var helper = new DataTypeConvertorHelper();
 
             var filter = "Antalya";
-            var path = string.Format("{0}\\sample_data.csv", ConfigurationManager.AppSettings["TestFiles"]);
-            var fileStream = new FileStream(path, FileMode.Open);
+            var path = string.Format("{0}\\sample_data.csv", TestFilesPath);
+            var exportpath = string.Format("{0}\\sample_data_export.xml", TestFilesPath);
 
-            Assert.IsTrue(helper.Process(fileStream, filter));
+            Assert.IsTrue(helper.Process(path, exportpath, filter));
         }
     }
 
     public class DataTypeConvertorHelper
     {
-        public bool Process(FileStream fileStream, string filter)
+        public bool Process(string path, string exportPath, string filter)
         {
-            throw new NotImplementedException();
+            var content = File.ReadAllLines(path);
+
+            var list = new List<AddressInfo>();
+            foreach (var line in content)
+            {
+                list.Add(new AddressInfo
+                {
+                    
+                });
+                
+            }
+
         }
     }
 }
