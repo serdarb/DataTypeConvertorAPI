19a174760dabc7a2a464bd923b5a2cc5a4cd335f
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-09-04 09:02:34 +0300

    test project setted up

---
+++ src/DataTypeConvertorAPI.Test/DataTypeConvertorServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DataTypeConvertorAPI.Test
+{
+
+    /* 
+     * 
+     *  Test case #1:
+     *      Generate XML output from CSV input,
+     *      filtered by City name = ’Antalya’
+     *  
+     */
+
+    [TestFixture]
+    public class DataTypeConvertorHelperTests
+    {
+        [Test]
+        public void should_generate_xml_from_csv()
+        {
+            var helper = new DataTypeConvertorHelper();
+
+            var filter = "Antalya";
+            var path = string.Format("{0}\\sample_data.csv", ConfigurationManager.AppSettings["TestFiles"]);
+            var fileStream = new FileStream(path, FileMode.Open);
+
+            Assert.IsTrue(helper.Process(fileStream, filter));
+        }
+    }
+
+    public class DataTypeConvertorHelper
+    {
+        public bool Process(FileStream fileStream, string filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
